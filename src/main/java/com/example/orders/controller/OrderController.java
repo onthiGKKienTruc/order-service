9a8b6d271080d88667cabaad1b3ba8fe0a7303e5
hien123454 <ngochien15092000@gmail.com>
2021-10-29 00:10:03 +0700

    commit config-cloud

--- src/main/java/com/example/orders/controller/OrderController.java
+++ src/main/java/com/example/orders/controller/OrderController.java
@@ -3,12 +3,15 @@ package com.example.orders.controller;
 import com.example.orders.entity.Order;
 import com.example.orders.service.OrderService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/orders")
 public class OrderController {
 
+    @Value("${welcome}")
+    String AA;
     @Autowired
     private OrderService orderService;
 
@@ -19,5 +22,9 @@ public class OrderController {
     @GetMapping("/{id}")
     public Order findOrderById(@PathVariable("id")Long orderId){
         return orderService.findOrderById(orderId);
+    }
+    @GetMapping("/")
+    public String helloWorld(){
+        return AA;
     }
 }
