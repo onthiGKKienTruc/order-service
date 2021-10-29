81cad5be7ba31711495b3e2cb0abde5d24a87f65
hien123454 <ngochien15092000@gmail.com>
2021-10-29 16:12:24 +0700

    commit master lan 2

--- src/main/java/com/example/orders/ordersApplication.java
+++ src/main/java/com/example/orders/ordersApplication.java
@@ -6,7 +6,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 
 @SpringBootApplication
-@EnableEurekaClient
 public class ordersApplication {
     public static void main(String[] args) {
         SpringApplication.run(ordersApplication.class, args);
